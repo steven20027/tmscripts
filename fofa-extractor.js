5fa90eab33dc791deda6d40d2e22603d65e5d2a1
Steven <steven20027@163.com>
2025-08-14 09:06:16 +0800

    Update fofa-extractor.js

--- fofa-extractor.js
+++ fofa-extractor.js
@@ -11,34 +11,42 @@
 (function () {
   'use strict';
 
-  function findUrls() {
-    const spans = document.querySelectorAll('span[data-clipboard-text]');
-    const urls = new Set();
+  function findUrls(){
+		const spans = document.querySelectorAll('span[data-clipboard-text]:not([style*="display: none"]):not([style*="display:none"])');
 
-    for (const span of spans) {
-      const style = window.getComputedStyle(span);
-      if (style.display === 'none') continue;
+		const values = Array.from(spans)
+		  .map(span => {
+			let text = span.dataset.clipboardText || '';
 
-      let text = span.dataset.clipboardText?.trim();
-      if (!text) continue;
+			const index = text.lastIndexOf('//');
+			text = index >= 0 ? text.slice(index + 2) : text;
+			return text.split(':')[0];
+		  })
+		  .filter(text => text !== '')  // 可选：去掉空字符串
+		  .filter((text, index, array) => array.indexOf(text) === index); // 去重
 
-      // 去除 // 之前的部分
-      const hostAndPath = text.includes('//')
-        ? text.slice(text.lastIndexOf('//') + 2)
-        : text;
+		return result;
+	}
 
-      // 去除端口号
-      const host = hostAndPath.split(':')[0];
+	function findIps() {
+		const links = document.querySelectorAll('div.hsxa-meta-data-item p:not(.hsxa-one-line) a[class="hsxa-jump-a"]');
+		const texts = [];
+		for (const link of links) {
+		  const style = window.getComputedStyle(link);
+		  if (style.display === 'none') continue; // 排除实际 display: none 的元素
 
-      if (host) urls.add(host);
-    }
+		  const text = link.textContent.trim();
+		  if (text && !texts.includes(text)) {
+			texts.push(text);
+		  }
+		}
 
-    return urls.size > 0 ? [...urls].join('\n') + '\n' : '\n';
-  }
+		return result;
+	}
 
   // 页面加载完成后执行
   setTimeout(() => {
-    const result = findUrls();
+    const result = [...findUrls(), ...findIps()].join('\n') ;
     console.log('[FOFA Hosts Extracted]:\n', result);
 
     // 可选：弹出结果
